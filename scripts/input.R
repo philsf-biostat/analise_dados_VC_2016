195fab6fdad1418fe62e308e18635da264086982
Felipe Figueiredo <philsf79@gmail.com>
2016-11-01 23:36:48 -0200

    Formatar datas

--- scripts/input.R
+++ scripts/input.R
@@ -2,6 +2,10 @@ dados <- read.csv2("../2016-11-01 TVP.csv", na.strings = "")
 
 dados <- dados[-c(2,5)] # Remover Nome e Cirurgia
 
+dados$Data.Exame <- as.Date(dados$Data.Exame, "%d/%m/%Y")
+dados$Data.Cirurgia <- as.Date(dados$Data.Cirurgia, "%d/%m/%Y")
+dados$Data.Atendimento <- as.Date(dados$Data.Atendimento, "%d/%m/%Y")
+
 dados$Prontuario <- factor(dados$Prontuario)
 dados <- dados[-c(1)] # Remover Prontuario
 dados$ANO <- factor(dados$ANO)
