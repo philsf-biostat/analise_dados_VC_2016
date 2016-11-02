0b57a0e842ba2c441c31695affc96fcc0339aba7
Felipe Figueiredo <philsf79@gmail.com>
2016-11-01 23:36:31 -0200

    Remover prontuário do working dataset

--- scripts/input.R
+++ scripts/input.R
@@ -3,6 +3,7 @@ dados <- read.csv2("../2016-11-01 TVP.csv", na.strings = "")
 dados <- dados[-c(2,5)] # Remover Nome e Cirurgia
 
 dados$Prontuario <- factor(dados$Prontuario)
+dados <- dados[-c(1)] # Remover Prontuario
 dados$ANO <- factor(dados$ANO)
 dados$IMC <- dados$Peso/(dados$Altura^2)
 
