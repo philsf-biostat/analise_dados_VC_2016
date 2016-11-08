18fdc574358e8387361e8d651551b47d9ab0bd24
Felipe Figueiredo <philsf79@gmail.com>
2016-11-08 21:54:35 -0200

    print separado da criação das tabelas (+tabela full)

--- scripts/descritivas.R
+++ scripts/descritivas.R
@@ -1,15 +1,19 @@
 library(tableone)
 vars <-  c("Altura", "Peso", "IMC", "Tipo.Atendimento", "TVP", "TEP", "SPT", "TVP.PREVIA")
 tabela1 <- CreateTableOne(data = dados, vars = vars)
-print(tabela1, showAllLevels = T)
 rm(vars)
 
 vars <- c("Rivoraxabana", "Dabigatrana", "Warfarina", "Enoxaparina")
 medicamentos <- CreateTableOne(data = dados, vars = vars)
-print(medicamentos, showAllLevels = T)
 rm(vars)
 
 vars <- c("HAS", "Doenca.Reumatica","Artrite.Reumatoide", "AVC", "DM", "Cardiopatia", "OUTRAS")
 comorbidades <- CreateTableOne(data = dados, vars = vars)
-print(comorbidades, showAllLevels = T)
 rm(vars)
+
+# tabela0 <- CreateTableOne(data = dados)
+# print(tabela0, showAllLevels = T)
+
+print(tabela1, showAllLevels = T)
+print(medicamentos, showAllLevels = T)
+print(comorbidades, showAllLevels = T)
