62618f48791fa7df96dc5b1697d48ba81ef4a545
Felipe Figueiredo <philsf79@gmail.com>
2016-11-12 20:42:17 -0200

    média e DP traduzidos on the fly

--- scripts/resultados.R
+++ scripts/resultados.R
@@ -7,6 +7,8 @@ panderOptions('table.style', 'rmarkdown')
 # tabela 1 ----------------------------------------------------------------
 
 tab1.output <- capture.output(pander(print(tabela1, showAllLevels = T, printToggle = F)))
+tab1.output <- gsub("mean", "média", tab1.output)
+tab1.output <- gsub("\\(sd\\)", "(DP)", tab1.output)
 cat(tab1.output, file = "resultados/tabela1.md", sep = "\n")
 
 # medicamentos ------------------------------------------------------------
@@ -18,6 +20,3 @@ cat(med.output, file = "resultados/medicamentos.md", sep = "\n")
 
 comorb.output <- capture.output(pander(print(comorbidades, showAllLevels = T, printToggle = F)))
 cat(comorb.output, file = "resultados/comorbidades.md", sep = "\n")
-
-system("sed -i s/mean/média/ resultados/*.md")
-system("sed -i s/sd/DP/ resultados/*.md")
