e6bfeb6d9323671e70a64556cb26a08a694da714
Felipe Figueiredo <philsf79@gmail.com>
2016-11-12 20:51:09 -0200

    resultados salvos em script próprio
    Agora, o resultados.R apenas gera as tabelas de resultados, prontos para
    print(). Estes podem ser usados diretamente no Rmarkdown (espero)

---
+++ scripts/resultados-output.R
@@ -0,0 +1,17 @@
+cat(tab1.output, file = "resultados/tabela1.md", sep = "\n")
+cat(med.output, file = "resultados/medicamentos.md", sep = "\n")
+cat(comorb.output, file = "resultados/comorbidades.md", sep = "\n")
+
+library(knitr)
+
+# tabela 1 ----------------------------------------------------------------
+
+pandoc("resultados/tabela1.md", format = c("latex", "docx"))
+
+# medicamentos ------------------------------------------------------------
+
+pandoc("resultados/medicamentos.md", format = c("latex", "docx"))
+
+# comorbidades ------------------------------------------------------------
+
+pandoc("resultados/comorbidades.md", format = c("latex", "docx"))
--- scripts/resultados.R
+++ scripts/resultados.R
@@ -9,14 +9,11 @@ panderOptions('table.style', 'rmarkdown')
 tab1.output <- capture.output(pander(print(tabela1, showAllLevels = T, printToggle = F)))
 tab1.output <- gsub("mean", "média", tab1.output)
 tab1.output <- gsub("\\(sd\\)", "(DP)", tab1.output)
-cat(tab1.output, file = "resultados/tabela1.md", sep = "\n")
 
 # medicamentos ------------------------------------------------------------
 
 med.output <- capture.output(pander(print(medicamentos, showAllLevels = T, printToggle = F)))
-cat(med.output, file = "resultados/medicamentos.md", sep = "\n")
 
 # comorbidades ------------------------------------------------------------
 
 comorb.output <- capture.output(pander(print(comorbidades, showAllLevels = T, printToggle = F)))
-cat(comorb.output, file = "resultados/comorbidades.md", sep = "\n")
