e23a74e06a4204e381332a2721517080b4b2e31d
Felipe Figueiredo <philsf79@gmail.com>
2016-11-12 19:52:54 -0200

    removido output para uso no Rmarkdown

--- scripts/input.R
+++ scripts/input.R
@@ -35,11 +35,3 @@ dados$IMC <- dados$Peso/(dados$Altura^2)
 
 ## Idade
 dados$Idade <- apply(dados,1,function(x) { length(seq.Date( as.Date(x['Nascimento']), as.Date(x['Data.Exame']), by='years')) } )
-
-str(dados)
-summary(dados)
-
-# attach(dados)
-# 
-# detach(dados)
-# rm(dados)
