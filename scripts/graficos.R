a0d24cd48615a070b1d6c77db829e60cccc76fae
Felipe Figueiredo <philsf79@gmail.com>
2016-11-13 04:47:19 -0200

    gráfico: Centros de Atendimento Especializado

--- scripts/graficos.R
+++ scripts/graficos.R
@@ -15,6 +15,9 @@ t.comorb <- cbind(
   OUTRAS = comorbidades$CatTable$Overall$OUTRAS$freq
 )
 
+# Centro <- sort(table(dados$Grupo), decreasing = T)
+Centro <- table(dados$Grupo)
+
 png("graficos/medicamentos.png", 700, 700)
 barplot(t.med, beside = T, legend.text = T, ylab = "Quantidade", xlab = "Medicamento")
 title("Profilaxia medicamentosa")
@@ -25,7 +28,10 @@ barplot(t.comorb, beside = T, legend.text = T, ylab = "Quantidade", xlab = "Como
 title("Presença de comorbidade")
 dev.off()
 
-png("graficos/centro.png", 700, 700)
-barplot(table(dados$Grupo), beside = T, ylab = "Quantidade", xlab = "Centro")
+png("graficos/centros.png", 700, 700)
+par(mar = c(7, 4, 4, 2) + 0.2) #add room for the rotated labels
+cp <- barplot(Centro, axes = FALSE, axisnames = FALSE, ylab = "Quantidade")
+text(cp, par("usr")[3], labels = rownames(Centro), srt = 60, adj = c(1.1,1.1), xpd = TRUE, cex = .6)
+axis(2)
 title("Casos por Centro de Atenção Especializada")
 dev.off()
