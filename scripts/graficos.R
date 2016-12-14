6d2aca901c22b45b756aaa35db0c77079d643703
Felipe Figueiredo <philsf79@gmail.com>
2016-12-14 08:46:35 -0200

    barplots: cores

--- scripts/graficos.R
+++ scripts/graficos.R
@@ -20,18 +20,18 @@ Centro <- sort(table(dados$Grupo), decreasing = T)
 # Centro <- table(dados$Grupo)
 
 png("graficos/medicamentos.png", 700, 700)
-barplot(t.med, beside = T, legend.text = c("NÃO", "SIM"), ylab = "Quantidade", xlab = "Medicamento")
+barplot(t.med, beside = T, legend.text = c("NÃO", "SIM"), ylab = "Quantidade", xlab = "Medicamento", col = rainbow(2, .6, .6))
 title("Profilaxia medicamentosa")
 dev.off()
 
 png("graficos/comorbidades.png", 700, 700)
-barplot(t.comorb, beside = T, legend.text = T, ylab = "Quantidade", xlab = "Comorbidade")
+barplot(t.comorb, beside = T, legend.text = T, ylab = "Quantidade", xlab = "Comorbidade", col = rainbow(2, .6, .6))
 title("Presença de comorbidade")
 dev.off()
 
 png("graficos/centros.png", 700, 700)
 par(mar = c(7, 4, 4, 2) + 0.2) #add room for the rotated labels
-cp <- barplot(Centro, axes = FALSE, axisnames = FALSE, ylab = "Quantidade")
+cp <- barplot(Centro, axes = FALSE, axisnames = FALSE, ylab = "Quantidade", col = rainbow(13, .6, .6))
 text(cp, par("usr")[3], labels = rownames(Centro), srt = 60, adj = c(1.1,1.1), xpd = TRUE, cex = .6)
 axis(2)
 title("Casos por Centro de Atenção Especializada")
