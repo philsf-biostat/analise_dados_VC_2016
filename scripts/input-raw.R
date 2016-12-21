d229505d950171170110c14cdbf5584881928450
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2016-12-21 17:12:14 -0200

    cruft removido

--- scripts/input-raw.R
+++ scripts/input-raw.R
@@ -1,8 +1,5 @@
 library(xlsx)
 # dados.seq <- read.xlsx("../2016-10-26 TVP REV  2010 11 12 13 14.xlsx",1)
 
-
-# str(dados.raw)
-
 write.table(dados.raw, "dataset/vc-dados.dat")
-rm(dados.raw, dados.seq)
+rm(dados.raw)
