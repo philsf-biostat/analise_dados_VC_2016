376d70f0df7db690dfe54a8c3af61a05b46a6371
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2016-12-21 17:20:32 -0200

    ler direto do XLSX (bug: codificação errada)

--- scripts/input-raw.R
+++ scripts/input-raw.R
@@ -1,5 +1,7 @@
 library(xlsx)
-# dados.seq <- read.xlsx("../2016-10-26 TVP REV  2010 11 12 13 14.xlsx",1)
 
-write.table(dados.raw, "dataset/vc-dados.dat")
+# a codificação dos dados está equivocada. nem latin1 nem utf8 funcionaram
+dados.raw <- read.xlsx("../2016-12-06_TVP.xlsx",1 , encoding = "utf8")
+
+write.csv2(dados.raw, "dataset/vc-dados.csv")
 rm(dados.raw)
