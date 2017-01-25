0bd6e8550be938b49b147a6871f3b455ac539bee
Felipe Figueiredo <philsf79@gmail.com>
2017-01-25 03:36:17 -0200

    nomes das colunas "n" ajustados

--- scripts/descritivas.R
+++ scripts/descritivas.R
@@ -5,7 +5,7 @@ tabela1 <- CreateTableOne(data = dados, vars = vars, strata = "Genero")
 tabela1$ContTable$F[,1:2] <- tabela1$ContTable$F[,1:2] - tabela1$ContTable$F[,2]
 tabela1$ContTable$M[,1:2] <- tabela1$ContTable$M[,1:2] - tabela1$ContTable$M[,2]
 tab1 <- as.data.frame(print(tabela1, printToggle = F, exact = T))
-tab1 <- cbind(tab1, n.F = c("n.F", tabela1$ContTable$F[,1]), n.M = c("n.M",tabela1$ContTable$M[,1]) )
+tab1 <- cbind(tab1, n.F = c("n", tabela1$ContTable$F[,1]), n.M = c("n",tabela1$ContTable$M[,1]) )
 tab1 <- tab1[c("n.F", "F", "n.M", "M", "p")]
 rownames(tab1) <- c("", "Idade (média (DP)", "Altura (média (DP)", "Peso (média (DP)", "IMC (média (DP)" )
 colnames(tab1) <- c("", "F", "", "M", "p")
