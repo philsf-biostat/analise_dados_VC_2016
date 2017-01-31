c1a753a26ce04765793baebfbde5e5690c5a03b9
Felipe Figueiredo <philsf79@gmail.com>
2017-01-31 21:57:29 -0200

    mybarplot(): função separada em script próprio

--- scripts/graficos.R
+++ scripts/graficos.R
@@ -1,36 +1,4 @@
-mybarplot <- function(tab, desfecho = NULL, preditor = NULL) {
-  par(mar = c(7, 5, 4, 2) + 0.1) #add room for the rotated labels
-  main <- paste(desfecho, "por", preditor)
-  col <- rev(rainbow(length((rownames(tab))), .6, .6))
-  barplot(tab,
-          beside = T,
-          col = col,
-          cex.axis = 1.2,
-          cex.lab = 1.6,
-          cex.names = 1.5,
-          cex.sub = 1.2,
-          ylim = c(0, 1.2*max(tab)),
-          xlab = desfecho,
-          ylab = "Número de pacientes")
-  if ( is.matrix(tab) ) {
-  mtext( paste("p-valor:",
-              format.pval(fisher.test(tab, workspace = 2e+6)$p.value,
-                          eps = .001,
-                          digits = 2)
-              ), cex = 1.3)
-    legend("topright",
-           rownames(tab),
-           title = preditor,
-           fill = col
-    )
-  }
-  else
-    main <- desfecho
-  title(
-    main = main,
-    cex.main = 2,
-  )
-}
+source("scripts/mybarplot.R")
 
 png("graficos/comorbidades-genero.png", 800, 800)
 par(mfrow = c(3,3))
